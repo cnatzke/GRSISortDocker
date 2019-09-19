1a6d99e48d10bceb624271a4dabcceb795ddb94e
cnatzke <natzke41@hotmail.com>
2019-09-19 13:14:44 -0600

    Fix graphics and Update .bashrc

--- bashrc
+++ bashrc
@@ -1,5 +1,21 @@
 ## .bashrc/.zshrc
 
+# Ignore Case for <tab> completion
+bind "set completion-ignore-case on"
+
+# Colour settings
+alias ls="ls -v --color=always --group-directories-first"
+alias grep="grep --color=auto"
 export PYTHONSTARTUP=$HOME/.pythonrc.py 
-export PS1='[\t] \e[91m\u\e[0m@\e[34m\h \e[36m\w \e[0m$ \'
-export PROMPT='[%*] %F{red}%n%f@%F{blue}%m%f %F{yellow}%3~%f $ '
+
+# Aliases for ease of use
+alias c='clear'
+alias q='exit'
+alias bashrc='vim /home/cnatzke/.bashrc'
+alias srcBash='source /home/cnatzke/.bashrc'
+alias rt='root -l'
+
+PS1='\e[38;5;241m(\u@\h):\w\n\[\e[0m\]--> '
+
+#export PS1='[\t] \e[91m\u\e[0m@\e[34m\h \e[36m\w \e[0m$ \'
+#export PROMPT='[%*] %F{red}%n%f@%F{blue}%m%f %F{yellow}%3~%f $ '
--- root_start.sh
+++ root_start.sh
@@ -1,18 +1,12 @@
 #!/bin/bash 
 
-    #--hostname="root_container" \
-    #--user=$(id -u) \
-    #--volume="/etc/group:/etc/group:ro" \
-    #--volume="/etc/passwd:/etc/passwd:ro" \
-    #--volume="/etc/shadow:/etc/shadow:ro" \
-    #--volume="/etc/sudoers.d:/etc/sudoers.d:ro" \
-    
 docker run -it --rm \
     --env="DISPLAY" \
+    --net=host \
     --hostname="root_container" \
     --user=$(id -u) \
-    --workdir="/home/$USER" \
-    --volume="/home/$USER:/home/$USER" \
+    --volume=$(pwd):$(pwd) \
+    --workdir="$(pwd)" \
     --volume="/etc/group:/etc/group:ro" \
     --volume="/etc/passwd:/etc/passwd:ro" \
     --volume="/etc/shadow:/etc/shadow:ro" \
@@ -20,4 +14,3 @@ docker run -it --rm \
     --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
     cnatzke/root-cern:latest
 
-# sudo apt update && sudo apt install -y x11-apps
